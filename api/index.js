6ed394abaf842d623c7e6a581de40d4df6547733
Shreya Kochar <60591774+skochar1@users.noreply.github.com>
2024-04-10 00:59:38 -0700

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ const Buffer = require('buffer').Buffer;
 const app = express();
 
 app.use(cors({
-    origin: 'https://skochar1.github.io',
-    methods: ['GET', 'POST'], // Specify allowed methods
-    credentials: true
+    origin: 'https://skochar1.github.io', // Specify allowed origin(s)
+    methods: ['GET', 'POST', 'OPTIONS'], // Specify allowed methods
+    allowedHeaders: ['Content-Type'], // Specify allowed headers
+    credentials: true, // If your frontend needs to send cookies or authentication info
+    optionsSuccessStatus: 200
 }));
 app.use(express.json());
 app.use(express.json());
