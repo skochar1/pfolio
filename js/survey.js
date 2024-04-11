6145089cf3d48e5ec9fbefc1857b6674812b3659
Shreya Kochar <60591774+skochar1@users.noreply.github.com>
2024-04-10 21:28:52 -0700

    Update survey.js

--- js/survey.js
+++ js/survey.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('randomQuizButton');
     
     button.addEventListener('click', function() {
-        fetch('https://script.google.com/macros/s/AKfycbzMcVE57VKyY5pZSGbHvbkRMBq8zFI57cGq7POll7_YVSSU5-CETqLCts_Q4u_nkvsFRQ/exec')
+        fetch('https://script.google.com/macros/s/AKfycbwgHxqwy-fEp3JtFZ8sg-OdFDC-laH-ZEaKfZt8TJkatDjxCqfdM767uGyh3rWwuZSGkA/exec')
             .then(response => response.text())
             .then(url => {
                 // Redirect the user to the fetched URL
