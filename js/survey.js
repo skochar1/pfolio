54c2ad2204ac0de0a6c289910e0f4c82233781ef
Shreya Kochar <60591774+skochar1@users.noreply.github.com>
2024-04-10 21:40:37 -0700

    Add files via upload

--- js/survey.js
+++ js/survey.js
@@ -2,12 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('randomQuizButton');
     
     button.addEventListener('click', function() {
+        // Disable the button immediately after it's clicked
+        button.disabled = true;
+        
+        // Fetch the URL from your Apps Script app
         fetch('https://script.google.com/macros/s/AKfycbwgHxqwy-fEp3JtFZ8sg-OdFDC-laH-ZEaKfZt8TJkatDjxCqfdM767uGyh3rWwuZSGkA/exec')
             .then(response => response.text())
             .then(url => {
                 // Redirect the user to the fetched URL
                 window.location.href = url;
             })
-            .catch(error => console.error('Error fetching form URL:', error));
+            .catch(error => {
+                console.error('Error fetching form URL:', error);
+            })
+            .finally(() => {
+                // Re-enable the button after 30 seconds, regardless of whether the fetch was successful or not
+                setTimeout(() => {
+                    button.disabled = false;
+                }, 30000); // 30000 milliseconds = 30 seconds
+            });
     });
 });
